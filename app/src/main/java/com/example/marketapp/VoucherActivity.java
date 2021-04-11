36382d72228995ffbe9dc30462344c27b9d381f3
Tiago Maia <jtdmaia@gmail.com>
2021-04-11 17:58:43 +0100

    Added contact points with REST API

--- app/src/main/java/com/example/marketapp/VoucherActivity.java
+++ app/src/main/java/com/example/marketapp/VoucherActivity.java
@@ -2,118 +2,55 @@ package com.example.marketapp;
 
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 import com.example.marketapp.model.Voucher;
+import com.example.marketapp.model.APICalls;
 import java.util.ArrayList;
 import java.util.List;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class VoucherActivity extends ListActivity {
-    private String DatabaseAddress = null;
-    private List listVouchers = new ArrayList<Voucher>();
+
+    private static String UserID = "1";
+    private static List vouchers;
+    private APICalls apiCalls;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        apiCalls = new APICalls();
         setContentView(R.layout.voucher);
         getAvailableVouchers();
+    }
 
+    public static void setVoucherList(ArrayList<Voucher> listVouchers) {
+        vouchers = listVouchers;
     }
 
     //api call to get available vouchers
     public void getAvailableVouchers() {
 
-        listVouchers.add("voucher 1");
-        listVouchers.add("voucher 2");
-        listVouchers.add("voucher 3");
+         getAllVouchers();
 
-        getAllVouchers();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         ArrayAdapter adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
-                listVouchers);
+                vouchers);
 
         setListAdapter(adapter);
     };
 
     public void getAllVouchers()
     {
-        GetUsers getUsers = new GetUsers(DatabaseAddress);
-        Thread thr = new Thread(getUsers);
+        APICalls.GetVouchers getVouchers = apiCalls.new GetVouchers(UserID);
+        Thread thr = new Thread(getVouchers);
         thr.start();
     }
 
-    private String readStream(InputStream in) {
-        BufferedReader reader = null;
-        StringBuilder response = new StringBuilder();
-        try {
-            reader = new BufferedReader(new InputStreamReader(in));
-            String line = "";
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
-            }
-        }
-        catch (IOException e) {
-            return e.getMessage();
-        }
-        finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                }
-                catch (IOException e) {
-                    return e.getMessage();
-                }
-            }
-        }
-        return response.toString();
-    }
-
-    private class GetUsers implements Runnable {
-        String address = null;
-
-        GetUsers(String baseAddress) {
-            address = baseAddress;
-        }
-
-        @Override
-        public void run() {
-            URL url;
-            HttpURLConnection urlConnection = null;
-
-            try {
-                url = new URL("http://" + address + ":8701/Rest/users");
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setDoInput(true);
-                urlConnection.setRequestProperty("Content-Type", "application/json");
-                urlConnection.setUseCaches (false);
-
-                int responseCode = urlConnection.getResponseCode();
-                if(responseCode == 200) {
-                    String response = readStream(urlConnection.getInputStream());
-                    Log.d("Log: ",response);
-                    listVouchers.add(new Voucher(null, null));
-                }
-                else
-                    Log.d("Log: ", String.valueOf(responseCode));
-            }
-            catch (Exception e) {
-                Log.d("Log: ", e.toString());
-            }
-            finally {
-                if(urlConnection != null)
-                    urlConnection.disconnect();
-            }
-        }
-    }
 }
---
+++ app/src/main/java/com/example/marketapp/model/APICalls.java
@@ -0,0 +1,239 @@
+package com.example.marketapp.model;
+
+import android.util.Log;
+
+import com.example.marketapp.CartItemsActivity;
+import com.example.marketapp.PastTransactionsActivity;
+import com.example.marketapp.VoucherActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.json.*;
+
+public class APICalls {
+
+    private void GetVouchersList(String jsonString) throws JSONException {
+        ArrayList<Voucher> voucherList = new ArrayList<>();
+        JSONObject obj = null;
+        try {
+            obj = new JSONObject(jsonString);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JSONArray arr = obj.getJSONArray("data");
+
+        for (int i = 0; i < arr.length(); i++)
+        {
+            JSONObject jsonObject = arr.getJSONObject(i);
+            long voucherID = jsonObject.getLong("id");
+            String voucherCODE = jsonObject.getString("code");
+            int discount = jsonObject.getInt("discount");
+            voucherList.add(new Voucher((long) voucherID, voucherCODE, discount));
+        }
+
+        VoucherActivity.setVoucherList(voucherList);
+    }
+
+    private void GetAllCartsList(String jsonString) throws JSONException {
+        ArrayList<Transaction> cartsList = new ArrayList<>();
+        JSONObject obj = null;
+        try {
+            obj = new JSONObject(jsonString);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JSONArray arr = obj.getJSONArray("data");
+
+        for (int i = 0; i < arr.length(); i++)
+        {
+            String voucherValue = arr.getJSONObject(i).getString("discount");
+            String date = arr.getJSONObject(i).getString("added_on");
+            long cartID = arr.getJSONObject(i).getLong("id");
+            long value = arr.getJSONObject(i).getLong("total");
+
+            cartsList.add(new Transaction(voucherValue, cartID, value, date));
+        }
+
+        PastTransactionsActivity.setAllCarsList(cartsList);
+    }
+
+    private void GetProductsInsideCart(String jsonString) throws JSONException {
+        ArrayList<Product> productsList = new ArrayList<>();
+        JSONObject obj = null;
+        try {
+            obj = new JSONObject(jsonString);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JSONArray arr = obj.getJSONArray("data");
+
+        for (int i = 0; i < arr.length(); i++)
+        {
+            //Long productID = arr.getJSONObject(i).getLong("voucher_id");
+            String name = arr.getJSONObject(i).getString("added_on");
+            Double value = arr.getJSONObject(i).getDouble("value");
+
+            productsList.add(new Product((Long.valueOf(i)), name, value));
+        }
+
+        CartItemsActivity.setListOfProducts(productsList);
+    }
+
+    private String readStream(InputStream in) {
+        BufferedReader reader = null;
+        StringBuilder response = new StringBuilder();
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        }
+        catch (IOException e) {
+            return e.getMessage();
+        }
+        finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                }
+                catch (IOException e) {
+                    return e.getMessage();
+                }
+            }
+        }
+        return response.toString();
+    }
+
+    //-----------------------------------------------------------------------------
+    // internal classes to deal with the REST operations
+
+    public class GetVouchers implements Runnable {
+        String address = "https://mesw-market-api.herokuapp.com/voucher/";
+
+        String userID;
+
+        public GetVouchers(String userID) {
+            this.userID = userID;
+        }
+
+        @Override
+        public void run() {
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(address + userID);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoInput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setUseCaches (false);
+
+                int responseCode = urlConnection.getResponseCode();
+                if(responseCode == 200) {
+                    String response = readStream(urlConnection.getInputStream());
+                    Log.d("Log: ",response);
+                    GetVouchersList(response);
+                }
+                else
+                    Log.d("Log: ", String.valueOf(responseCode));
+            }
+            catch (Exception e) {
+                Log.d("Log: ", e.toString());
+            }
+            finally {
+                if(urlConnection != null)
+                    urlConnection.disconnect();
+            }
+        }
+    }
+
+    public class GetAllCarts implements Runnable {
+        String address = "https://mesw-market-api.herokuapp.com/cart/summary/";
+        String userID;
+
+        public GetAllCarts(String userID) {
+            this.userID = userID;
+        }
+
+        @Override
+        public void run() {
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(address + userID);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoInput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setUseCaches (false);
+
+                int responseCode = urlConnection.getResponseCode();
+                if(responseCode == 200) {
+                    String response = readStream(urlConnection.getInputStream());
+                    Log.d("Log: ",response);
+                    GetAllCartsList(response);
+                }
+                else
+                    Log.d("Log: ", String.valueOf(responseCode));
+            }
+            catch (Exception e) {
+                Log.d("Log: ", e.toString());
+            }
+            finally {
+                if(urlConnection != null)
+                    urlConnection.disconnect();
+            }
+        }
+    }
+
+
+    public class GetProducts implements Runnable {
+        String address = "https://mesw-market-api.herokuapp.com/cart/detail/";
+        String cartID;
+
+        public GetProducts(String cartID) {
+            this.cartID = cartID;
+        }
+
+        @Override
+        public void run() {
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(address + cartID);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoInput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setUseCaches (false);
+
+                int responseCode = urlConnection.getResponseCode();
+                if(responseCode == 200) {
+                    String response = readStream(urlConnection.getInputStream());
+                    Log.d("Log: ",response);
+                    GetProductsInsideCart(response);
+                }
+                else
+                    Log.d("Log: ", String.valueOf(responseCode));
+            }
+            catch (Exception e) {
+                Log.d("Log: ", e.toString());
+            }
+            finally {
+                if(urlConnection != null)
+                    urlConnection.disconnect();
+            }
+        }
+    }
+
+}
